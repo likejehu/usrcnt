9099bdc889a7ba5c87aeaebd36388aee65223efc
likejehu <etqash@gmail.com>
2020-04-18 07:16:54 +0300

    add some logic to handler

--- handlers/handler.go
+++ handlers/handler.go
@@ -20,8 +20,8 @@ type Handler struct {
 
 //Hello is handler that creates new session and deals with logic
 func (h *Handler) Hello(w http.ResponseWriter, r *http.Request) {
-
-	// Create a new random session token
+	usrCountkey := "usrcountkey"
+	// Create a new random session token with uuid
 	sessionToken := uuid.NewV4().String()
 	// Set the token in the cache, along with the user whom it represents
 	// The token has an expiry time of 120 seconds
@@ -32,15 +32,15 @@ func (h *Handler) Hello(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Finally, we set the client cookie for "session_token" as the session token we just generated
-	// we also set an expiry time of 120 seconds, the same as the cache
+	//Set the client cookie for "session_token" as the session token
+	// set an expiry time of 120 seconds
 	http.SetCookie(w, &http.Cookie{
 		Name:    "session_token",
 		Value:   sessionToken,
 		Expires: time.Now().Add(120 * time.Second),
 	})
 
-	// We can obtain the session token from the requests cookies, which come with every request
+	// obtain the session token from the requests cookies, which come with every request
 	c, err := r.Cookie("session_token")
 	if err != nil {
 		if err == http.ErrNoCookie {
@@ -54,7 +54,7 @@ func (h *Handler) Hello(w http.ResponseWriter, r *http.Request) {
 	}
 	sessionToken = c.Value
 
-	// We then get the name of the user from our cache, where we set the session token
+	// get the name of the user from our cache, where we set the session token
 	response, err := h.Cache.Do("GET", sessionToken)
 	if err != nil {
 		// If there is an error fetching from cache, return an internal server error status
@@ -66,6 +66,7 @@ func (h *Handler) Hello(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
-	// Finally, return the welcome message to the user
+	usrCountVal, err := h.Cache.Do("INCR", usrCountkey)
 	w.Write([]byte(fmt.Sprintf("Welcome %s!", response)))
+	w.Write([]byte(fmt.Sprintf("usercount: %v", usrCountVal)))
 }
